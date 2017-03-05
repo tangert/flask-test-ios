5c0f936ccbf4fc7e6140e44203b9aa82b8f08980
tangert <tyler@angert.com>
2017-03-05 17:18:33 -0500

    initial commit

--- flask-test/ViewController.swift
+++ flask-test/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fetchDataButton: UIButton!
+    @IBOutlet weak var dataLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    @IBAction func fetchData(_ sender: Any) {
+        let r = Just.get("https://flask-test-1.herokuapp.com/post-test")
+        if r.ok && r.statusCode == 200 {
+            let json = JSON(r.json!)
+            self.dataLabel.text = json["data"].string
+        }
     }
 
     override func didReceiveMemoryWarning() {
